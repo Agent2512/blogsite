71ad8f759789154389c19e646f6816398de7f31e
niklas gadeberg <nikl2207@tietgen.dk>
2020-10-22 12:41:16 +0200

    add does to login page

--- login.php
+++ login.php
@@ -2,7 +2,6 @@
 require("./template/head.php"); ?>
 <?php require("./template/nav.php"); ?>
 <?php require("./template/msg.php"); ?>
-<!-- div.row.w-100.h-100>.col.w-50.h-50*2 -->
 <div class="w-100 h-90 container-fluid">
     <div class="row w-100 h-100">
         <!-- col1 start -->
@@ -14,10 +13,12 @@ require("./template/head.php"); ?>
                 </div>
                 <!-- form start -->
                 <form class="w-75 align-self-center" method="POST" action="script/userLogin.php">
+                    <!-- username form group -->
                     <div class="form-group">
                         <label for="loginUsername">Username</label>
                         <input type="username" class="form-control" id="loginUsername" name="username" placeholder="Username" required>
                     </div>
+                    <!-- password form group -->
                     <div class="form-group">
                         <label for="loginPassword">Password</label>
                         <input type="password" class="form-control" id="loginPassword" name="password" placeholder="Password" required>
@@ -38,14 +39,17 @@ require("./template/head.php"); ?>
                 </div>
                 <!-- form start -->
                 <form class="w-75 align-self-center" method="POST" action="script/userRegister.php">
+                    <!-- username form group -->
                     <div class="form-group">
                         <label for="registerUsername">Username</label>
                         <input type="username" class="form-control" id="registerUsername" name="username" placeholder="Username" required>
                     </div>
+                    <!-- email form group -->
                     <div class="form-group">
                         <label for="registerEmail">Username</label>
-                        <input type="email" class="form-control" id="registerEmail" name="email"placeholder="Email" required>
+                        <input type="email" class="form-control" id="registerEmail" name="email" placeholder="Email" required>
                     </div>
+                    <!-- password form group -->
                     <div class="form-group">
                         <label for="registerPassword">Password</label>
                         <input type="password" class="form-control" id="registerPassword" name="password" placeholder="Password" required>
