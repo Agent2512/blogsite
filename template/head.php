6348d2dc3dbb04f4e69a911d3575d0d5d6f52e41
niklas gadeberg <nikl2207@tietgen.dk>
2020-10-22 10:08:34 +0200

    add main.css to haed

--- template/head.php
+++ template/head.php
@@ -9,6 +9,10 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+    <!-- custom css -->
+    <link rel="stylesheet" href="css/main.css">
+
+    <!-- title to page -->
     <title><?php echo $pageTitle ?></title>
 </head>
 
