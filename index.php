f7b6cacc434a30e4af880d834df529b50672cc2c
niklas gadeberg <nikl2207@tietgen.dk>
2020-10-22 09:53:43 +0200

    does: add does to card temlate

--- index.php
+++ index.php
@@ -9,19 +9,25 @@ require("./template/head.php"); ?>
 
     <div class="card m-2">
         <img src="img/temp.svg" alt="" class="card-img-top">
+        <!-- card title -->
         <div class="card-header">ælaksd</div>
         <div class="card-body">
+            <!-- card text -->
             <p class="card-text">asdasdgasdasdasd</p>
+            <!-- card btn to card main page -->
             <a href="#" class="btn btn-primary w-100">Go somewhere</a>
         </div>
         <div class="card-body border-top">
+            <!-- card category list -->
             <div class="d-inline-flex border rounded p-1">
                 <p class="m-0">test</p>
             </div>
             
         </div>
         <div class="card-footer text-muted">
+            <!-- card creator -->
             <p class="card-text">by: Agent2512</p>
+            <!-- card timestamp -->
             <p class="card-text">14:30 20-10-2020</p>
         </div>
     </div>
