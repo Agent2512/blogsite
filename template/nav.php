40f039a035df24f03721543ec143ca72467afc01
agent2512 <spiler2512@live.dk>
2020-10-22 20:19:08 +0200

    make and add logout script

--- template/nav.php
+++ template/nav.php
@@ -1,15 +1,42 @@
-<nav class="navbar navbar-expand navbar-dark bg-primary">
-    <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="./index.php">Home</a>
+<nav class='navbar navbar-expand navbar-dark bg-primary'>
+    <div class='collapse navbar-collapse' id='navbarText'>
+        <ul class='navbar-nav mr-auto'>
+            <li class='nav-item active'>
+                <a class='nav-link' href='./index.php'>Home</a>
             </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="./makeblog.php">make blog</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="./login.php">login</a>
-            </li>
+            <?php 
+                if(!isset($_SESSION)) {
+                    session_start();
+                }
+            ?>
+            <?php 
+                if(isset($_SESSION["username"])) {
+                    echo "
+                    <li class='nav-item active'>
+                        <a class='nav-link' href='./makeblog.php'>make blog</a>
+                    </li>
+                    ";
+                    echo "
+                    <li class='nav-item active'>
+                        <a class='nav-link' href='./profile.php'>profile</a>
+                    </li>
+                    ";
+                    echo "
+                    <li class='nav-item active'>
+                        <a class='nav-link' href='./script/userLogout.php'>logout</a>
+                    </li>
+                    ";
+                }
+            ?>
+            <?php 
+                if(!isset($_SESSION["username"])) {
+                    echo "
+                    <li class='nav-item active'>
+                        <a class='nav-link' href='./login.php'>login</a>
+                    </li>
+                    ";
+                }
+            ?>
         </ul>
     </div>
 </nav>
