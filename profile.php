25ce92f69fa906f389b85fe2690b947ed81e3ece
agent2512 <spiler2512@live.dk>
2020-10-22 19:58:04 +0200

    make profile.php

---
+++ profile.php
@@ -0,0 +1,7 @@
+<?php $pageTitle = "make blog"; require("./template/head.php");?>
+<?php require("./template/nav.php");?>
+<?php require("./template/msg.php");?>
+
+    <h1>profile</h1>
+
+<?php require("./template/footer.php");?>
