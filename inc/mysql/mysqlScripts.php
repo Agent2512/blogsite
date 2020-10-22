9cc0dea22322c87f5cb2c909eafbb648b2106ae1
niklas gadeberg <nikl2207@tietgen.dk>
2020-10-22 14:11:23 +0200

    add getAllUsers and createUser to mysqlScripts

--- inc/mysql/mysqlScripts.php
+++ inc/mysql/mysqlScripts.php
@@ -1,5 +1,17 @@
 <?php
 class mysqlScripts extends mysql
 {
-    
+    public function getAllUsers()
+    {
+        return $this->getData("
+            SELECT * FROM `login`
+        ");
+    }
+
+    public function createUser(string $username, string $password, string $email)
+    {
+        return $this->createData("
+        INSERT INTO `login` (`id`, `username`, `password`, `email`) VALUES (NULL, '$username', '$password', '$email');
+        ");
+    }
 }
