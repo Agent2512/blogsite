4d92f27756cc3a2b0e7927d84230f5d1fe1c9f35
niklas gadeberg <nikl2207@tietgen.dk>
2020-10-26 09:50:49 +0100

    fix code lineup

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
-<?php $pageTitle = "login";
-require("./template/head.php"); ?>
+<?php $pageTitle = "login"; require("./template/head.php"); ?>
 <?php require("./template/nav.php"); ?>
 <?php require("./template/msg.php"); ?>
 
