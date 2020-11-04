abac45264d1aa4c7c794b8ba3d8f14b502db5d27
niklas gadeberg <nikl2207@tietgen.dk>
2020-11-04 14:53:19 +0100

    done with login and register parts

--- inc/form_validator.php
+++ inc/form_validator.php
@@ -11,7 +11,7 @@ class form_validator {
             "username",
             "password",
             "email"
-            ]
+        ]
     ];
 
     public function __construct($post_data){
@@ -19,9 +19,9 @@ class form_validator {
     }
 
     public function validateForm($fieldKey){
-        if ($fieldKey == "login") {
-            $fields = $this->fields[$fieldKey];
+        $fields = $this->fields[$fieldKey];
 
+        if ($fieldKey == "login") {
             foreach($fields as $field) {
                 if (!array_key_exists($field, $this->data)) {
                     trigger_error("$field is not parent in data");
@@ -29,31 +29,74 @@ class form_validator {
                 }
             }
 
-            $this->validateUsername();
+            $this->validateUsername($fieldKey);
+            $this->validatePassword($fieldKey);
         }
+        else if ($fieldKey == "register") {
+            foreach($fields as $field) {
+                if (!array_key_exists($field, $this->data)) {
+                    trigger_error("$field is not parent in data");
+                    return;
+                }
+            }
+
+            $this->validateUsername($fieldKey);
+            $this->validatePassword($fieldKey);
+            $this->validateEmail($fieldKey);
+        }
+
+        return $this->errors;
     }
 
-    private function validateUsername()
+    private function validateUsername(string $addErrorId = "")
     {
         $val = trim($this->data['username']);
 
+        if ($addErrorId != "") {
+            $addErrorId = ucfirst($addErrorId);
+        }
+
         if (empty($val)) {
-            $this->addError("username", "username cannot be empty");
+            $this->addError("username$addErrorId", "username cannot be empty");
         } else {
-            if (preg_match('/^[a-zA-Z0-9]{1,255}$/', $val)) {
-                # code...
+            if (!preg_match('/^[a-zA-Z0-9]{6,255}$/', $val)) {
+                $this->addError("username$addErrorId", "username must be 6-255 char & alphanumeric");
             }
         }
     }
 
-    private function validatePassword()
+    private function validatePassword(string $addErrorId = "")
     {
-        # code...
+        $val = trim($this->data['password']);
+
+        if ($addErrorId != "") {
+            $addErrorId = ucfirst($addErrorId);
+        }
+
+        if (empty($val)) {
+            $this->addError("password$addErrorId", "password cannot be empty");
+        } else {
+            if (strlen($val >= 8)) {
+                $this->addError("password$addErrorId", "password must be 8 char or more");
+            }
+        }
     }
 
-    private function validateEmail()
+    private function validateEmail(string $addErrorId = "")
     {
-        # code...
+        $val = trim($this->data['email']);
+
+        if ($addErrorId != "") {
+            $addErrorId = ucfirst($addErrorId);
+        }
+
+        if (empty($val)) {
+            $this->addError("email$addErrorId", "email cannot be empty");
+        } else {
+            if (!filter_var($val, FILTER_VALIDATE_EMAIL)) {
+                $this->addError("email$addErrorId", "email must be a valid email");
+            }
+        }
     }
 
     private function addError($key, $val)
