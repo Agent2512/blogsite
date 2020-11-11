92afd23b2e898d2f2eec66995e130102f4c1d07a
niklas gadeberg <nikl2207@tietgen.dk>
2020-11-11 14:46:30 +0100

    markup to image upload

--- test.php
+++ test.php
@@ -1,7 +1,42 @@
 <?php
-var_dump($_POST);
+$target_dir = "./img/uploads/";
+
+function getDirFilenames($target)
+{
+    $temp = scandir($target);
+    unset($temp[0]);
+    unset($temp[1]);
+    $temp = array_values($temp);
+
+    for ($i = 0; $i < count($temp); $i++) {
+        $temp[$i] = explode(".", $temp[$i])[0];
+    }
+
+    return $temp;
+}
+
+$new_file = $_FILES["image"];
+$new_file_name = explode(".", $new_file["name"])[0];
+$new_file_extension = "." . explode(".", $new_file["name"])[1];
+
+$i = 0;
+while (in_array($new_file_name, getDirFilenames($target_dir))) {
+    if (in_array($new_file_name . $i, getDirFilenames($target_dir)) == false) {
+        $new_file_name = $new_file_name . $i;
+
+        break;
+    }
+    $i++;
+}
+
+move_uploaded_file($new_file["tmp_name"], $target_dir . $new_file_name . $new_file_extension);
+
+print_r("<pre>");
+print_r($_FILES);
+print_r("</pre>");
+
 echo "<br>";
-var_dump($_FILES);
-$file=$_FILES['image']['name'];
-echo "<br>";
-echo move_uploaded_file($_FILES['image']['tmp_name'],"./img/uploads/".$file);
+
+print_r("<pre>");
+print_r(getDirFilenames($target_dir));
+print_r("</pre>");
