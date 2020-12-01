a4ab9d3b66579a6afb7527a69f477442d73f3faf
niklas gadeberg <nikl2207@tietgen.dk>
2020-12-01 13:13:43 +0100

    add some code comments to blog page file

--- blog.php
+++ blog.php
@@ -18,10 +18,15 @@ $blog = $db->getBlogById($_GET["id"]);
 $categories = $db->getAllCategoriesToBlog($_GET["id"]);
 $comments = $db->getAllCommentsToBlog($_GET["id"]);
 // add comment to the blog and reload the page
+// checks if user is logged in
 if (isset($_SESSION["username"])) {
+    // checks if there is a new comment
     if (isset($_POST["comment"])) {
+        // checks the comment is not empty and not to long
         if (!empty($_POST["comment"]) && strlen($_POST["comment"]) <= 256) {
+            // adds comment to database
             $db->createComment($_SESSION["username"], $_GET["id"], $_POST["comment"]);
+            // reloads the page 
             header("Location: ./blog.php?id=" . $_GET["id"]);
         } else $commentError = "max allowed characters is 255 or empty";
     }
