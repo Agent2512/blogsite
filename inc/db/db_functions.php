96cdd1b8d3d7f19b014b9cd570830e44976cc447
niklas gadeberg <nikl2207@tietgen.dk>
2020-12-15 14:05:21 +0100

    db_functions done with docblock

--- inc/db/db_functions.php
+++ inc/db/db_functions.php
@@ -2,6 +2,13 @@
 class db_functions extends db_connection
 {
     // user_control
+
+    /**
+     * gets all users from database id, username, email and password
+     * 
+     * @return false|array returns false if an error occurred or the return form database is empty,
+     * returns array if database return is one or more items
+     */
     public function getAllUsers()
     {
         return $this->getData("
@@ -9,6 +16,17 @@ class db_functions extends db_connection
         ");
     }
 
+    /**
+     * creates a new user in database
+     * 
+     * @param string $username the name of user
+     * 
+     * @param string $password the password of user
+     * 
+     * @param string $email the email of user
+     * 
+     * @return bool returns true if successful returns false if user can not created
+     */
     public function createUser(string $username, string $password, string $email)
     {
         return $this->createData("
@@ -16,11 +34,30 @@ class db_functions extends db_connection
         ");
     }
 
+    /**
+     * gets userID bu string:username
+     * 
+     * @param string $username for the user you want ID on
+     * 
+     * @return false|string returns false if an error occurred or the return form database is empty,
+     * returns string if database return successful
+     */
     public function getUserId(string $username)
     {
-        return $this->getData("SELECT id  FROM `login` WHERE username = '$username'")[0]['id'];
+        $x = $this->getData("SELECT id  FROM `login` WHERE username = '$username'");
+
+        if ($x == false) return false;
+        else return $x[0]['id'];
     }
     // Categories_control
+
+    /**
+     * removes all categories on one blog
+     * 
+     * @param string $blogId the ID of the blog to removes all categories
+     * 
+     * @return bool returns false if an error occurred returns true if successful 
+     */
     public function deleteAllCategoriesToBlog(string $blogId)
     {
         $this->deleteData("
@@ -28,28 +65,58 @@ class db_functions extends db_connection
         ");
     }
 
+    /**
+     * gets all categories on form database
+     * 
+     * @return false|array returns false if an error occurred or the return form database is empty, 
+     * returns array if database return is one or more items
+     */
     public function getAllCategories()
     {
         $x = $this->getData("SELECT * FROM `categories`");
 
-        if ($x == false) {
-            return $x;
-        }
-        else {
-            return array_column($x, "name");
-        }
+        if ($x == false) return $x;
+        else return array_column($x, "name");
     }
 
+    /**
+     * gets the ID of category
+     * 
+     * @param string $categoryName the name of category to ID
+     * 
+     * @return false|string returns false if an error occurred or the return form database is empty,
+     * returns string if database return successful
+     */
     public function getCategoryID(string $categoryName)
     {
-        return $this->getData("SELECT id FROM `categories` WHERE name = '$categoryName'")[0]["id"];
+        $x = $this->getData("SELECT id FROM `categories` WHERE name = '$categoryName'");
+
+        if ($x == false) return false;
+        else return $x[0]['id'];
     }
 
+    /**
+     * adds one category to a blog in database
+     * 
+     * @param string $blogId the id of blog
+     * 
+     * @param string $categoryId the ID of the category to addCategoryToBlog
+     * 
+     * @return bool returns true if successful to add category to blog
+     */
     public function addCategoryToBlog(string $blogId, string $categoryId)
     {
-        $this->createData("INSERT INTO `blogs_has_categories` (`id`, `blogId`, `categoryId`) VALUES (NULL, '$blogId', '$categoryId')");
+        return $this->createData("INSERT INTO `blogs_has_categories` (`id`, `blogId`, `categoryId`) VALUES (NULL, '$blogId', '$categoryId')");
     }
 
+    /**
+     * gets all categories to one blog in database
+     * 
+     * @param string $blogId the ID of the blog to get categories
+     * 
+     * @return false|array returns false if an error occurred or the return form database is empty,
+     * returns array if database return is one or more items
+     */
     public function getAllCategoriesToBlog(string $blogId)
     {
         $x = $this->getData("
@@ -59,37 +126,76 @@ class db_functions extends db_connection
         WHERE blogs_has_categories.blogId = $blogId
         ");
 
-        if ($x == false) {
-            return $x;
-        }
-        else {
-            return array_column($x, "name");
-        }
+        if ($x == false) return false;
+        else return array_column($x, "name");
     }
     // file_control
+    // nothing to do with database
+
+    /**
+     * finds a available image name so there is not two file name the same
+     * 
+     * @param array $image_data the $_FILES data off a POST event
+     * 
+     * @return string the new name of the image to use on server
+     */
+    public function getAvailableImageName(array $image_data)
+    {
+        // splits data in to two smaller values
+        $new_file_name = explode(".", $image_data["image"]["name"])[0];
+        $new_file_extension = "." . explode(".", $image_data["image"]["name"])[1];
+
+        // keeps adding a number to image name 
+        $i = 0;
+        while (in_array($new_file_name, $this->getAllUploadImageNames())) {
+            if (in_array($new_file_name . $i, $this->getAllUploadImageNames()) == false) {
+                return $new_file_name . $i . $new_file_extension;
+            }
+            $i++;
+        }
+    }
+
+    /**
+     * remove all unused files from ./img/uploads
+     * compared to fileNames on database
+     */
     public function deleteUnusedFiles()
     {
-        $base = array_column($this->getData("SELECT Image FROM `blogs`"),"Image");
+        // gets all fileName from database
+        $base = array_column($this->getData("SELECT Image FROM `blogs`"), "Image");
 
-        $local = scandir("./img/uploads/");
-        unset($local[0]);
-        unset($local[1]);
-        $local = array_values($local);
+        // scans dir
+        $dir = scandir("./img/uploads/");
+        unset($dir[0]); // removed "."
+        unset($dir[1]); // removed ".."
+        $dir = array_values($dir); // re-index array
 
-        $result = array_values(array_diff($local, $base));
+        // finds the difference between database and dir 
+        $result = array_values(array_diff($dir, $base));
 
-        for ($i=0; $i < count($result); $i++) { 
+        // deletes all files in dir not used in database
+        for ($i = 0; $i < count($result); $i++) {
             unlink("./img/uploads/$result[$i]");
         }
     }
-    
+
+    /**
+     * get a array with all dir fileName 
+     * no file execution
+     * 
+     * @param string $target default: "./img/uploads/" can be changed
+     * 
+     * @return array off file in $target dir with no file execution
+     */
     private function getAllUploadImageNames(string $target = "./img/uploads/")
     {
-        $dir = scandir($target);
-        unset($dir[0]);
-        unset($dir[1]);
-        $dir = array_values($dir);
+        // scans dir
+        $dir = scandir("./img/uploads/");
+        unset($dir[0]); // removed "."
+        unset($dir[1]); // removed ".."
+        $dir = array_values($dir); // re-index array
 
+        // removes file executions
         for ($i = 0; $i < count($dir); $i++) {
             $dir[$i] = explode(".", $dir[$i])[0];
         }
@@ -97,58 +203,136 @@ class db_functions extends db_connection
         return $dir;
     }
 
+    /**
+     * uploads file to set dir
+     * 
+     * @param string $fileLocation the location of the you want to upload
+     * 
+     * @param string $fileName what should the name be on the file
+     * 
+     * @param string $fileDestination what dir should the file end op
+     * 
+     * @return bool returns true if successful
+     */
     private function uploadImage($fileLocation, $fileName, $fileDestination = "./img/uploads/")
     {
-        move_uploaded_file($fileLocation, $fileDestination . $fileName);
+        return move_uploaded_file($fileLocation, $fileDestination . $fileName);
     }
     // comment_control
-    public function createComment(string $username, string $blog_id, string $comment)
+    /**
+     * makes a comment to a blog in database
+     * 
+     * @param string $username the username of comment
+     * 
+     * @param string $blogId the ID of the blog to add comment
+     * 
+     * @param string $comment is the text of the comment
+     * 
+     * @return bool returns true if successful
+     */
+    public function createComment(string $username, string $blogId, string $comment)
     {
         $user_id = $this->getUserId($username);
+        if ($user_id == false) return false;
 
-        $this->createData("INSERT INTO `comments` (`id`, `text`, `blog_id`, `user_id`, `timestamp`) VALUES (NULL, '$comment', '$blog_id', '$user_id', current_timestamp());");
+        return $this->createData("INSERT INTO `comments` (`id`, `text`, `blog_id`, `user_id`, `timestamp`) VALUES (NULL, '$comment', '$blogId', '$user_id', current_timestamp());");
     }
 
-    public function getAllCommentsToBlog(string $blog_id)
+    /**
+     * gets all comments for one blog
+     * 
+     * @param string $blogId the ID of the blog to get all comments
+     * 
+     * @return false|array returns false if an error occurred or the return form database is empty,
+     * returns array if database return is one or more items
+     */
+    public function getAllCommentsToBlog(string $blogId)
     {
         return $this->getData("
         SELECT comments.id, comments.timestamp, comments.text, login.username
         FROM `comments`
         INNER JOIN login ON login.id = comments.user_id
-        WHERE comments.blog_id = '$blog_id'
+        WHERE comments.blog_id = '$blogId'
         ");
     }
-    
-    public function getCommentById(string $comment_id)
+
+    /**
+     * gets comments by ID 
+     * 
+     * @param string $commentId the ID of the comment to return
+     * 
+     * @return false|array returns false if an error occurred or the return form database is empty,
+     * returns array if database return is one
+     */
+    public function getCommentById(string $commentId)
     {
         $x = $this->getData("
         SELECT comments.id, comments.text, comments.blog_id, comments.user_id, comments.timestamp, login.username
         FROM `comments`
         INNER JOIN login on login.id = comments.user_id
-        WHERE comments.id = '$comment_id'
+        WHERE comments.id = '$commentId'
         ");
 
-        if ($x == false) return $x;
+        if ($x == false) return false;
         else return $x[0];
     }
 
-    public function deleteCommentById(string $comment_id)
+    /**
+     * deletes one comment by ID
+     * 
+     * @param string $commentId the ID of the comment to delete
+     * 
+     * @return bool returns true if successful
+     */
+    public function deleteCommentById(string $commentId)
     {
-        $this->deleteData("DELETE FROM `comments` WHERE id = '$comment_id'");
+        return $this->deleteData("DELETE FROM `comments` WHERE id = '$commentId'");
     }
     // blog_control
+
+    /**
+     * deletes one blog by ID
+     * 
+     * @param string $id the ID of the blog to delete 
+     * 
+     * @return bool returns true if successful
+     */
     public function deleteBlogByID(string $id)
     {
-        $this->deleteData("DELETE FROM `blogs` WHERE id = '$id'");
+        $x = $this->deleteData("DELETE FROM `blogs` WHERE id = '$id'");
+
+        $this->deleteUnusedFiles();
+
+        return $x;
     }
 
+    /**
+     * get a ine blog in database with $title and $username and no ID
+     * 
+     * @param string $title the title of blog
+     * 
+     * @param string $username who created the blog
+     * 
+     * @return false|array returns false if an error occurred or the return form database is empty,
+     * returns array if database return is one
+     */
     public function getBlogOneData(string $title, string $username)
     {
         $userId = $this->getUserId($username);
+        if ($userId == false) return false;
 
-        return $this->getData("SELECT * FROM `blogs` WHERE user_id = '$userId' && title = '$title'")[0];
+        $x = $this->getData("SELECT * FROM `blogs` WHERE user_id = '$userId' && title = '$title'");
+
+        if ($x == false) return false;
+        else return $x[0];
     }
 
+    /**
+     * gets all blog in database
+     * 
+     * @return false|array returns false if an error occurred or the return form database is empty,
+     * returns array if database return is one or more items
+     */
     public function getAllBlogs()
     {
         return $this->getData("
@@ -157,18 +341,35 @@ class db_functions extends db_connection
         INNER JOIN login on login.id = blogs.user_id
         ");
     }
+
+    /**
+     * get all blogs images names with no execution
+     * 
+     * @return false|array returns false if an error occurred or the return form database is empty,
+     * returns array if database return is one or more items
+     */
     public function getAllBlogImageNames()
     {
         $data = $this->getData("SELECT Image FROM `blogs`");
+        if ($data == false) return false;
+
         $data = array_column($data, "Image");
 
         for ($i = 0; $i < count($data); $i++) {
             $data[$i] = explode(".", $data[$i])[0];
         }
 
-        return  $data;
+        return $data;
     }
 
+    /**
+     * gets one blog by ID from database
+     * 
+     * @param string $blogId the ID of the blog
+     * 
+     * @return false|array returns false if an error occurred or the return form database is empty,
+     * returns array if database return is one
+     */
     public function getBlogById(string $blogId)
     {
         $x = $this->getData("
@@ -178,61 +379,68 @@ class db_functions extends db_connection
         WHERE blogs.id = '$blogId'
         ");
 
-        if ($x == false) {
-            return $x;
-        }
-        else {
-            return $x[0];
-        }
+        if ($x == false) return false;
+        else return $x[0];
     }
 
+    /**
+     * makes a blog on the database with all data as categories and image
+     * 
+     * @param string $username the username of who is making a blog
+     * 
+     * @param array $post_data all text data the blog need
+     * 
+     * @param array $image_data all image data form $_FILES  
+     * 
+     * @return bool returns true if successful
+     */
     public function makeBlog(string $username, array $post_data, array $image_data)
     {
         $userId = $this->getUserId($username);
+        if ($userId == false) return false;
+
         $title = $post_data["title"];
         $decoration = $post_data["decoration"];
         $text = $post_data["text"];
         $categories = $post_data["categories"] ?? [];
 
-        $new_file = $image_data["image"];
-        $new_file_name = explode(".", $new_file["name"])[0];
-        $new_file_extension = "." . explode(".", $new_file["name"])[1];
+        $image = $this->getAvailableImageName($image_data);
 
-        $i = 0;
-        while (in_array($new_file_name, $this->getAllUploadImageNames())) {
-            if (in_array($new_file_name . $i, $this->getAllUploadImageNames()) == false) {
-                $new_file_name = $new_file_name . $i;
-
-                break;
-            }
-            $i++;
-        }
-        $image = $new_file_name . $new_file_extension;
-
-        $x1 = $this->createData("
+        $x = $this->createData("
         INSERT INTO `blogs` (`id`, `title`, `decoration`, `text`, `Image`, `user_id`, `timestamp`)
         VALUES (NULL, '$title', '$decoration', '$text', '$image', '$userId', current_timestamp());
         ");
 
-
-        if (count($categories) != 0 && $x1 == true) {
-            $blog = $this->getBlogOneData($title, $username);
-            $blogId = $blog["id"];
-
-            foreach ($categories as $category) {
-                $categoryId = $this->getCategoryId($category);
-
-                $this->addCategoryToBlog($blogId, $categoryId);
-            }
-        }
-
-        if ($x1 == true) {
-            $this->uploadImage($new_file["tmp_name"], $image);
+        if ($x == true) {
+            $uploadImage = $this->uploadImage($image_data["image"]["tmp_name"], $image);
+            if ($uploadImage == false) return false;
 
             $this->deleteUnusedFiles();
-        }
+
+            $blog = $this->getBlogOneData($title, $username);
+            if ($blog == false) return false;
+
+            if (count($categories) != 0) foreach ($categories as $category) {
+                $categoryId = $this->getCategoryId($category);
+
+                $this->addCategoryToBlog($blog["id"], $categoryId);
+            }
+
+            return true;
+        } else return false;
     }
 
+    /**
+     * edits a blog on the database with all data as categories and image
+     * 
+     * @param string $blogId the ID of the blog there need to be edited
+     * 
+     * @param array $post_data all text data the blog need
+     * 
+     * @param array $image_data all image data form $_FILES  
+     * 
+     * @return bool returns true if successful
+     */
     public function editBlog(string $blogId, array $post_data, array $image_data)
     {
         $title = $post_data["title"];
@@ -240,22 +448,9 @@ class db_functions extends db_connection
         $text = $post_data["text"];
         $categories = $post_data["categories"] ?? [];
 
-        $new_file = $image_data["image"];
-        $new_file_name = explode(".", $new_file["name"])[0];
-        $new_file_extension = "." . explode(".", $new_file["name"])[1];
+        $image = $this->getAvailableImageName($image_data["image"]);
 
-        $i = 0;
-        while (in_array($new_file_name, $this->getAllUploadImageNames())) {
-            if (in_array($new_file_name . $i, $this->getAllUploadImageNames()) == false) {
-                $new_file_name = $new_file_name . $i;
-
-                break;
-            }
-            $i++;
-        }
-        $image = $new_file_name . $new_file_extension;
-
-        $x1 = $this->updateData("
+        $x = $this->updateData("
         UPDATE `blogs` 
         SET title='$title', decoration='$decoration', text='$text',Image='$image' 
         WHERE id = '$blogId'
@@ -263,18 +458,19 @@ class db_functions extends db_connection
 
         $this->deleteAllCategoriesToBlog($blogId);
 
-        if (count($categories) != 0 && $x1 == true) {
-            foreach ($categories as $category) {
+        if ($x == true) {
+            $uploadImage = $this->uploadImage($image_data["image"]["tmp_name"], $image);
+            if ($uploadImage == false) return false;
+
+            $this->deleteUnusedFiles();
+
+            if (count($categories) != 0) foreach ($categories as $category) {
                 $categoryId = $this->getCategoryId($category);
 
                 $this->addCategoryToBlog($blogId, $categoryId);
             }
-        }
 
-        if ($x1 == true) {
-            $this->uploadImage($new_file["tmp_name"], $image);
-
-            $this->deleteUnusedFiles();
-        }
+            return true;
+        } else return false;
     }
 }
