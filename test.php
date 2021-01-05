c20545afbcec0bc90b5cdf49c598d333ee182b88
niklas gadeberg <nikl2207@tietgen.dk>
2021-01-05 10:40:19 +0100

    update test file

--- test.php
+++ test.php
@@ -1,44 +1,24 @@
 <?php
-$data = [
-    
-];
+require "./inc/inc.php";
 
-array_push($data, array("event_id" => "25-12-2000", "game_id" => "csgo", "result" => "test1"));
-array_push($data, array("event_id" => "25-12-2001", "game_id" => "acc", "result" => "test2"));
-array_push($data, array("event_id" => "25-12-2002", "game_id" => "acc", "result" => "test3"));
-array_push($data, array("event_id" => "25-12-2000", "game_id" => "csgo", "result" => "test4"));
-array_push($data, array("event_id" => "25-12-2002", "game_id" => "lol", "result" => "test5"));
-array_push($data, array("event_id" => "25-12-2003", "game_id" => "lol", "result" => "test6"));
-array_push($data, array("event_id" => "25-12-2001", "game_id" => "acc", "result" => "test7"));
-array_push($data, array("event_id" => "25-12-2002", "game_id" => "csgo", "result" => "test8"));
-array_push($data, array("event_id" => "25-12-2002", "game_id" => "lol", "result" => "test9"));
 
-$outputArray = [];
+$blog_control = new blog_control();
+$blogs = $blog_control->getAllBlogs();
+// $blog = $blog_control->getBlog("92");
 
-$events = [];
+echo "default";
+print_r("<pre>");
+print_r($blogs);
+print_r("</pre>");
+echo "<br> new";
 
-foreach ($data as $row) {
-    // gets all events
-    array_push($events, $row["event_id"]);
+if ($blogs != false) foreach ($blogs as $key => $blog) if ($blog["approved"] == 0) {
+    unset($blogs[$key]);
 }
-sort($events);
-$events = array_unique($events);
+$blogs = array_values($blogs);
 
-foreach ($events as $event) {
-    // make all events in the outputArray
-    $outputArray[$event] = array();
-}
-
-// add all game_id to the events
-foreach ($data as $row) {
-    $outputArray[$row["event_id"]][$row["game_id"]] = array();
-}
-
-// add all result to the game_id
-foreach ($data as $row) {
-    array_push($outputArray[$row["event_id"]][$row["game_id"]], $row["result"]);
-}
+// var_dump($blogs);
 
 print_r("<pre>");
-print_r($outputArray);
+print_r($blogs);
 print_r("</pre>");
